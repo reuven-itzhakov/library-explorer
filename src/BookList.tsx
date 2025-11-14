9059635b6f034af7474cc0a33a6486758558985f
Reuven Itzhakov <itzhakovreuven@gmail.com>
2025-11-14 13:14:30 +0200

    Add BookList and BookCard components; update App to use BookList and fetch books from the correct path

---
+++ src/BookList.tsx
@@ -0,0 +1,15 @@
+import {Book} from './types';
+import BookCard from './BookCard';
+
+function BookList({ books }: { books: Book[] }) {
+
+    return (
+        <>
+          {books.map((book, index) => (
+            <BookCard {...book} key={index} />
+          ))}
+        </>
+    );
+}
+
+export default BookList;
