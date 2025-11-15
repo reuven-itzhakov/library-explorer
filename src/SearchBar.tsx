363ca5aacceb374e1609326e1491f9d369ed3920
Reuven Itzhakov <itzhakovreuven@gmail.com>
2025-11-15 18:33:16 +0200

    Add SearchBar component styles; create SearchBar.css for improved layout and design

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import './Styles/SearchBar.css';
 
 /*
 Note: The search functionality is currently triggered by either the Search button or the "Enter" key.
@@ -13,7 +14,7 @@ function SearchBar({setSearchQuery}: {setSearchQuery: (query: string) => void}) 
     const [inputValue, setInputValue] = useState<string>("");
 
     return(
-        <div>
+        <div className="search-container">
             <input
                 type="text"
                 placeholder="Search titles or authors" 
@@ -26,9 +27,9 @@ function SearchBar({setSearchQuery}: {setSearchQuery: (query: string) => void}) 
                     }
                 }}
             />
-            <button onClick={() => setSearchQuery(inputValue)}>
+            <button className="search-button" onClick={() => setSearchQuery(inputValue)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none">
-                    <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
             </button>
         </div>
